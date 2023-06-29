5f0713788063b1ec7c339e321c638dd7743a9d17
Andrii Morozov <slon88888@ukr.net>
2023-06-29 23:08:35 +0300

    word array with symbols

--- script.js
+++ script.js
@@ -7,16 +7,17 @@ button.addEventListener('click', (event) => {
   event.preventDefault();
   console.clear();
   getWordArray();
-  getLetters();
+  getWords();
   form.reset();
 });
 function getWordArray() {
   stringArray = textArea.value.toLowerCase().split(n);
+  textArray = stringArray.reduce((acc, currentValue) => acc + space + currentValue);
+  wordArray = textArray.split(space);
+  wordArray = wordArray.filter(word => word.length > 0);
 }
-function getLetters() {
-  wordArray.forEach((element) => {
-    if (element.length !== 0) {
+function getWords() {
+  wordArray.forEach(element => {
       console.log(element);
-    }
   });
 }
