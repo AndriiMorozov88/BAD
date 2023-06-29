f15bc4cff33da408f8cc7531b0bb5a6dcd1324e9
Andrii Morozov <slon88888@ukr.net>
2023-06-29 12:32:18 +0300

    add html

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const text = 'The Tao gave birth to machine language. Machine language gave birthto the assembler. The assembler gave birth to the compiler. Now there are ten thousand languages. Each language has its purpose, however humble. Each language expresses the Yin and Yang of software. Each language has its place within the Tao. But do not program in COBOL if you can avoid it. -- Geoffrey James, "The Tao of Programming"'
 const space = ' ';
+const split = '';
 let wordArray = text.split(space);
 wordArray.forEach((word) => {
-  
+  word.split(split);
+  return(wordArray);
 });
 console.log(wordArray);
