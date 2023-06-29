f9549f640c4664b37542d2dacf00380d32e8bd73
Andrii Morozov <slon88888@ukr.net>
2023-06-30 01:06:27 +0300

    did until sorting symbols

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const space = ' ';
-const n = '\n';
+const newLine = '\n';
 const textArea = document.querySelector('[data-text-area]');
 const form = document.querySelector('[data-form]');
 const button = document.querySelector('[data-button]');
@@ -8,16 +8,14 @@ button.addEventListener('click', (event) => {
   console.clear();
   getWordArray();
   getWords();
-  form.reset();
+  //form.reset();
 });
 function getWordArray() {
-  stringArray = textArea.value.toLowerCase().split(n);
+  stringArray = textArea.value.toLowerCase().split(newLine);
   textArray = stringArray.reduce((acc, currentValue) => acc + space + currentValue);
   wordArray = textArray.split(space);
   wordArray = wordArray.filter(word => word.length > 0);
 }
 function getWords() {
-  wordArray.forEach(element => {
-      console.log(element);
-  });
+  
 }
