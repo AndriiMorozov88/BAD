39b13eed0fa98ceed318243c4094ba40e334446c
Andrii Morozov <slon88888@ukr.net>
2023-06-29 16:28:50 +0300

    added form

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const text = 'The Tao gave birth to machine language. Machine language gave birthto the assembler. The assembler gave birth to the compiler. Now there are ten thousand languages. Each language has its purpose, however humble. Each language expresses the Yin and Yang of software. Each language has its place within the Tao. But do not program in COBOL if you can avoid it. -- Geoffrey James, "The Tao of Programming"'
 const space = ' ';
-const split = '';
-let wordArray = text.split(space);
-wordArray.forEach((word) => {
-  word.split(split);
-  return(wordArray);
+const textArea = document.querySelector('[data-text-area]');
+const form = document.querySelector('[data-form]');
+const button = document.querySelector('[data-button]');
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.clear();
+  getWordArray();
+  getLetters();
+  form.reset();
 });
-console.log(wordArray);
+function getWordArray() {
+  wordArray = textArea.value.toLowerCase().split(space);
+}
+function getLetters() {
+  wordArray.forEach(element => console.log(element.length));
+}
