bd40337abe995c61240e7acf4c1558a3908e67f7
Andrii Morozov <slon88888@ukr.net>
2023-06-30 12:07:02 +0300

    added letter array

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const space = ' ';
 const newLine = '\n';
+const noSpace = '';
 const textArea = document.querySelector('[data-text-area]');
 const form = document.querySelector('[data-form]');
 const button = document.querySelector('[data-button]');
@@ -7,15 +8,23 @@ button.addEventListener('click', (event) => {
   event.preventDefault();
   console.clear();
   getWordArray();
-  getWords();
+  getLettersArray();
   //form.reset();
 });
 function getWordArray() {
   stringArray = textArea.value.toLowerCase().split(newLine);
   textArray = stringArray.reduce((acc, currentValue) => acc + space + currentValue);
   wordArray = textArray.split(space);
-  wordArray = wordArray.filter(word => word.length > 0);
 }
-function getWords() {
+function getLettersArray() {
+  letterArray = new Array();
+  wordArray.forEach(word => {
+    letterArray.push(word.split(noSpace));
+  });
   
+  for (let count = 0; count < letterArray.length; count++) {
+    letterArray[count] = letterArray[count].filter(symbol => symbol >= 'a' && symbol <= 'z');
+  }
+  letterArray = letterArray.filter(word => word.length > 0);
+  console.log(letterArray);
 }
