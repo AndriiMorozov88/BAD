1f9f0af0bb8a72d24b008458cb094ddac1da07a9
Andrii Morozov <slon88888@ukr.net>
2023-07-17 21:50:55 +0300

    added regExp

--- script.js
+++ script.js
@@ -8,23 +8,24 @@ button.addEventListener('click', (event) => {
   event.preventDefault();
   console.clear();
   getWordArray();
-  getLettersArray();
+  // getLettersArray();
+  // getFirstUnicalLettersArray();
   //form.reset();
 });
 function getWordArray() {
-  stringArray = textArea.value.toLowerCase().split(newLine);
-  textArray = stringArray.reduce((acc, currentValue) => acc + space + currentValue);
-  wordArray = textArray.split(space);
+  text = textArea.value.toLowerCase();
+  textEdited = text.replace(/[\n, \W]/g, ' ').replace(/\s{2,}/g, ' ');
+  console.log(textEdited);
 }
-function getLettersArray() {
-  letterArray = new Array();
-  wordArray.forEach(word => {
-    letterArray.push(word.split(noSpace));
-  });
+// function getLettersArray() {
+//   letterArray = new Array();
+//   wordArray.forEach(word => {
+//     letterArray.push(word.split(noSpace));
+//   });
   
-  for (let count = 0; count < letterArray.length; count++) {
-    letterArray[count] = letterArray[count].filter(symbol => symbol >= 'a' && symbol <= 'z');
-  }
-  letterArray = letterArray.filter(word => word.length > 0);
-  console.log(letterArray);
-}
+//   for (let count = 0; count < letterArray.length; count++) {
+//     letterArray[count] = letterArray[count].filter(symbol => symbol >= 'a' && symbol <= 'z');
+//   }
+//   letterArray = letterArray.filter(word => word.length > 0);
+// }
+
